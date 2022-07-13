00659fa934083ffd4b289da42740a1112f02421c
Trevor L Davis <trevor.l.davis@gmail.com>
2022-07-13 12:47:13 -0700

    feat: neovim beancount plugin

--- config/init_config.sh
+++ config/init_config.sh
@@ -32,3 +32,17 @@ UTILITIES_XDG_CONFIG_HOME=$UTILITIES_CONFIG_HOME/.config
 cd "$UTILITIES_XDG_CONFIG_HOME"
 find -type d -exec mkdir --parents -- "$XDG_CONFIG_HOME"/{} \;
 find -type f -exec ln --symbolic --interactive -- "$UTILITIES_XDG_CONFIG_HOME"/{} "$XDG_CONFIG_HOME"/{} \;
+cd ..
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+if ! [ -d $XDG_DATA_HOME ]
+then
+    mkdir $XDG_DATA_HOME
+    echo "Created $XDG_DATA_HOME"
+fi
+
+for file in $(ls .local/share)
+do
+    echo .local/share/$file
+    ln --symbolic --interactive $UTILITIES_CONFIG_HOME/.local/share/$file $XDG_DATA_HOME/$file
+done
